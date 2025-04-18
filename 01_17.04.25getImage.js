ddd9931f8b2ba3badd41532ea371fc77a1ca9984
WalterOswald <rolousch@gmail.com>
2025-04-18 11:32:28 +0200

    CreateImgObjBase

--- 01_17.04.25getImage.js
+++ 01_17.04.25getImage.js
@@ -1,27 +1,76 @@
-const fileSelect = document.getElementById("fileSelect"),
-    fileElem = document.getElementById("fileElem"),
-    preview = document.getElementById("preview");
+const fileSelect = document.getElementById("fileSelect"),//where you upload files
+    fileElem = document.getElementById("fileElem"),//<input id="fileElem" type="file" multiple />
+    preview = document.getElementById("preview"), slider1 = document.getElementById("slider1");//container
+
+let ImgObjName = ""
+let ImgObjWidth, ImgObjHeight, ImgObjBorderR
 
 
+
+
+
+const ImgArray = [];
 
 fileElem.addEventListener("change", handleFiles, false);
 
-function handleFiles() {
 
 
+//the "change" listens to wether a file was uploaded, the handle files function then awaits an (event)
+//in this case the "change" 
+
+function handleFiles(event) {
+    //creates an <ul> in the container "preview"
+    const files = event.target.files;   //.files is a FileList object, holds the selected files.
     const list = document.createElement("ul");
     preview.appendChild(list);
-    for (let i = 0; i < this.files.length; i++) {
-        const li = document.createElement("li");
-        list.appendChild(li);
 
-        const img = document.createElement("img");
-        img.src = URL.createObjectURL(this.files[i]);
-        img.height = 1 / 2;
-        li.appendChild(img);
-        const info = document.createElement("span");
-        info.textContent = `${this.files[i].name}: ${this.files[i].size} bytes`;
-        li.appendChild(info);
+
+    for (let i = 0; i < files.length; i++) {
+
+        const imageObj = new imageObject(files[i], 150, 150);
+        ImgObjName = ("ImgObj" + i)
+        ImgArray.push(imageObj);
+        imageObj.display(list);
+
+
+
+        //const img = document.createElement("img");
+        // img.src = URL.createObjectURL(this.files[i]);
+        //img.height = 100;
+        //ImgArray.push(img)
+    }
+
+
+
+}
+
+class imageObject {
+
+
+
+
+    constructor(file, ImgObjWidth = 20, ImgObjHeight = 20, ImgObjBorderR = 20) {
+        this.file = file;
+        this.img = document.createElement("img");
+        this.img.src = URL.createObjectURL(file);
+        this.img.width = ImgObjWidth;
+        this.img.height = ImgObjHeight;
+        this.ImgObjName = ImgObjName
+        this.ImgObjBorderR = ImgObjBorderR
+
+
+    }
+
+    changeStyle(imgFocus) {
+        this.imgFocus.setAttribute("style", "border-color:#FFFFFF;");
+    }
+
+    display(preview) {
+        preview.appendChild(this.img);
+        console.log(this.ImgObjName)
     }
 
 }
+
+
+
